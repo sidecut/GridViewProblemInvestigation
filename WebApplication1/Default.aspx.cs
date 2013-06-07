fa753b832c8e206b9bc2d6cbc7387b7a3fd51f61
Jim Raden <jim.raden@sherwin.com>
2013-06-07 15:32:15 -0400

    Move data binding back to Page_Load, but do not call DataBind() explicitly.

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -9,17 +9,11 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-        }
-
-        protected override void OnPreRender(EventArgs e)
-        {
-            base.OnPreRender(e);
-
             if (!IsPostBack)
             {
                 var people = GetData();
                 GridView1.DataSource = people;
-                GridView1.DataBind();
+                //GridView1.DataBind();
             }
         }
 
